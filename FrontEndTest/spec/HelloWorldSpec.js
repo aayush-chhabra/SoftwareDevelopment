d3561991e5da21fba4cc3e8ae8d62bcf52ce31d4
SameerSawla <sameer.sawla.cs@gmail.com>
2014-11-30 21:53:48 -0500

    Adding some more tests

---
+++ FrontEndTest/spec/HelloWorldSpec.js
@@ -0,0 +1,126 @@
+describe("Test Suite 1 ", function() {
+    it("Case 1.1", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 1.2", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 1.3", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 1.4", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 1.5", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 1.6", function() {
+        expect(1).toEqual(1);
+    });
+});
+
+describe("Test Suite 2 ", function() {
+    it("Case 2.1", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.2", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.3", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.4", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.5", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.6", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.7", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.8", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.9", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.10", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.11", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 2.12", function() {
+        expect(1).toEqual(1);
+    });
+});
+
+describe("Test Suite 3 ", function() {
+    it("Case 3.1", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.2", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.3", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.4", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.5", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.6", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.7", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.8", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.9", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.10", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.11", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("Case 3.12", function() {
+        expect(1).toEqual(1);
+    });
+});
+
---
+++ FrontEndTest/src/HelloWorld.js
@@ -0,0 +1,7 @@
+function helloWorld() {
+    return "Hello world!";
+}
+function multiply(one,two)
+{
+	return (one+1)*two;
+}
