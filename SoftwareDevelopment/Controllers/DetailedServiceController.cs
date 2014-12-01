79e13c0eced7a7620c9b988afc1482d5be523490
Aayush Chhabra <chhabraaayush@yahoo.ca>
2014-11-30 19:26:16 -0500

    Controllers, Models and Views (2)

---
+++ SoftwareDevelopment/Controllers/DetailedServiceController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Capstone.Models;
+
+namespace Capstone.Controllers
+{
+    public class DetailedServiceController : Controller
+    {
+        private CapstoneDb db = new CapstoneDb();
+        public static int _serviceArea = 0;
+
+        //
+        // GET: /DetailedService/
+
+        public ActionResult Index([Bind(Prefix="id")] int serviceAreaId)
+        {
+            var serviceArea = db.ServiceAreas.Find(serviceAreaId);
+            if (serviceArea != null)
+            {
+                _serviceArea = serviceAreaId;
+                return View(serviceArea);
+            }
+            return HttpNotFound();
+        }
+
+        //
+        // GET: /DetailedService/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            DetailedService detailedservice = db.DetailedServices.Find(id);
+            if (detailedservice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detailedservice);
+        }
+
+        //
+        // GET: /DetailedService/Create
+
+        [HttpGet]
+        public ActionResult Create(int serviceAreaId)
+        {
+            PopulateServiceProvidersDropDownList();
+            return View();
+        }
+
+        //
+        // POST: /DetailedService/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DetailedService detailedservice)
+        {
+            if (ModelState.IsValid)
+            {
+                var serviceArea = db.ServiceAreas.Find(_serviceArea);
+                detailedservice.ServiceArea = serviceArea;
+
+                detailedservice.ServiceProvider = db.Users.Find(detailedservice.UserId);
+                
+                db.DetailedServices.Add(detailedservice);
+                db.SaveChanges();
+                return RedirectToAction("Index", new { id = _serviceArea });
+            }
+
+            return View(detailedservice);
+        }
+
+        //
+        // GET: /DetailedService/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            DetailedService detailedservice = db.DetailedServices.Find(id);
+            if (detailedservice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detailedservice);
+        }
+
+        //
+        // POST: /DetailedService/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(DetailedService detailedservice)
+        {
+            if (ModelState.IsValid)
+            {
+                var serviceArea = db.ServiceAreas.Find(_serviceArea);
+                detailedservice.ServiceArea = serviceArea;
+
+                db.Entry(detailedservice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", new { id = _serviceArea });
+            }
+            return View(detailedservice);
+        }
+
+        //
+        // GET: /DetailedService/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            DetailedService detailedservice = db.DetailedServices.Find(id);
+            if (detailedservice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detailedservice);
+        }
+
+        //
+        // POST: /DetailedService/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DetailedService detailedservice = db.DetailedServices.Find(id);
+            db.DetailedServices.Remove(detailedservice);
+            db.SaveChanges();
+            return RedirectToAction("Index", new { id = _serviceArea });
+        }
+
+        [Authorize(Roles = "Administrator")]
+        private void PopulateServiceProvidersDropDownList(object selectedProviders = null)
+        {
+            string roleName = "Service Provider";
+            var serviceProviderList = Roles.GetUsersInRole(roleName);
+
+            List<SelectListItem> servicesQuery = new List<SelectListItem>();
+
+            foreach (var investigator in serviceProviderList)
+            {
+                var user = (from p in db.Users
+                            where p.UserName.Equals(investigator)
+                            select p.Id).Take(1);
+
+                var user1 = db.Users.Find((int)user.First());
+
+                servicesQuery.Add(new SelectListItem
+                {
+                    Text = user1.FullName,
+                    Value = user1.Id.ToString()
+                });
+            }
+
+            ViewBag.UserId = servicesQuery; // new SelectList(servicesQuery, "Id", "FullName", selectedProviders);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SoftwareDevelopment/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Capstone.Controllers
+{
+    public class HomeController : Controller
+    {
+        CapstoneDb _db = new CapstoneDb();
+
+        public ActionResult Index()
+        {
+            if (Roles.GetRolesForUser().Contains("Administrator"))
+            {
+                var projectsQuery = from v in _db.Projects
+                                    where !(v.Status.Equals("approved"))
+                                    select v;
+                return View(_db.Projects.ToList());
+            }
+
+            return View(_db.Projects.ToList());
+        }
+
+        private int getCurrentUserId()
+        {
+            MembershipUser user = Membership.GetUser(User.Identity.Name);
+
+            if (user == null)
+            {
+                return 0;
+            }
+            return (int)user.ProviderUserKey;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
